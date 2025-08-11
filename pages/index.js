ef7d74bcf0011d6e6dc0c5669bc4c9c28921c6fb
Stevar69 <stevar69@gmail.com>
2025-08-11 23:02:05 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Chathost is live!</h1>
+      <p>Je Next.js app draait nu op Vercel.</p>
+    </div>
+  );
+}
